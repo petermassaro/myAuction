e45f29ba1fe9409f23dbe56b654750c4350127fc
Pete <peter.j.massaro@gmail.com>
2018-12-30 21:12:01 -0500

    Added user model

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -4,6 +4,8 @@ from flask_login import login_user, logout_user, login_required, \
 from . import auth
 from .forms import LoginForm, RegistrationForm, ChangePasswordForm,\
     PasswordResetRequestForm, PasswordResetForm, ChangeEmailForm
+from .. import db
+from ..models import User
 
 
 @auth.route('/login', methods=['GET', 'POST'])
@@ -16,6 +18,24 @@ def login():
             next = request.args.get('next')
             if next is None or not next.startswith('/'):
                 next = url_for('main.index')
+            flash("Logged in as {}".format(user.username))
             return redirect(next)
         flash('Invalid username or password.')
     return render_template('auth/login.html', form=form)
+
+
+@auth.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email=form.email.data,
+                    username=form.username.data,
+                    password=form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        token = user.generate_confirmation_token()
+#        send_email(user.email, 'Confirm Your Account',
+#                   'auth/email/confirm', user=user, token=token)
+        flash('A confirmation email has been sent to you by email.')
+        return redirect(url_for('auth.login'))
+    return render_template('auth/register.html', form=form)
--- auction.py
+++ auction.py
@@ -14,17 +14,16 @@ if os.environ.get('FLASK_COVERAGE'):
 import sys
 import click
 from flask_migrate import Migrate, upgrade
-from app import create_app #Need to define create_app function
-#from app.models import * #Amend when models are created
+from app import create_app, db #Need to define create_app function
+from app.models import * #Amend when models are created
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
-#migrate = Migrate(app, db)
+migrate = Migrate(app, db)
 
 @app.shell_context_processor
 def make_shell_context():
 	#Implement after db models defined
-	pass
-
+    return dict(db=db, User=User)
 
 
 @app.cli.command()
